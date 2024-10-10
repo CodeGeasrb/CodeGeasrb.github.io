3d0e9d10a5b891e6c3b9bc064840e1f139e15ec3
CodeGeasrb <ririba@outlook.es>
2024-10-10 00:54:34 -0600

    Updating repository...

--- python/MyDataAnalysisProject/scripts/data_preprocessing.py
+++ python/MyDataAnalysisProject/scripts/data_preprocessing.py
@@ -1,7 +1,35 @@
+import sys
+import os
+
+sys.path.append('../data_analysis_project')
+
+from utils.fuctions import clean_comments_text, remove_stopwords
 
 
 
-
+stop_words_spanish = [
+    'a', 'al', 'algo', 'alguien', 'algún', 'alguna', 'algunas', 'algunos', 'ambos', 'ante', 'antes', 'aki', 'abajo', 
+    'bien', 'como', 'comó', 'con', 'cmo', 'cual', 'cuando', 'cuándo', 'donde', 'dónde', 'durante', 'el', 'él', 'ella', 
+    'ellas', 'ellos', 'en', 'entre', 'era', 'eras', 'éramos', 'eran', 'es', 'esa', 'esas', 'ese', 'esos', 'esta', 
+    'estaba', 'estabas', 'estaban', 'estamos', 'estar', 'este', 'esto', 'estos', 'estoi', 'toy', 'fue', 'fui', 
+    'fuese', 'fuesen', 'fueron', 'siendo', 'sino', 'son', 'soy', 'tal', 'tambn', 'tambien', 'también', 'tan', 'tanta', 
+    'tantas', 'tanto', 'tantos', 'te', 'ti', 'tienes', 'tiene', 'tienen', 'tu', 'tú', 'tus', 'un', 'uno', 'unos', 
+    'una', 'unas', 'veses', 'veces', 'y', 'ya', 'io', 'yo', 'mi', 'mí', 'mios', 'míos', 'tuyo', 'tuyos', 'suyo', 
+    'suyos', 'mio', 'nos', 'nosotros', 'vosotros', 'su', 'sí', 'si', 'les', 'los', 'nuestras', 'nuestras', 'nuestros', 
+    'otros', 'otra', 'otras', 'o', 'os', 'se', 'sé', 'asi', 'así', 'alli', 'allá', 'aka', 'aquí', 'ora', 'ahora', 
+    'apenas', 'ante', 'aun', 'aún', 'además', 'aunque', 'como', 'cómo', 'contra', 'cual', 'cuales', 'cuando', 
+    'cuándo', 'desde', 'de', 'despues', 'después', 'dice', 'dijo', 'dixo', 'dicho', 'donde', 'dónde', 'dos', 'durante', 
+    'e', 'él', 'ella', 'ellas', 'ellos', 'en', 'entre', 'era', 'eras', 'eran', 'es', 'esa', 'esas', 'ese', 'eso', 
+    'esos', 'esta', 'estan', 'están', 'estar', 'este', 'esto', 'estos', 'toy', 'toi', 'fue', 'fui', 'fueron', 
+    'fueron', 'fuesen', 'fui', 'hago', 'izo', 'hizo', 'hemos', 'asta', 'hasta', 'hay', 'iba', 'iban', 'iwal', 'igual', 
+    'incluso', 'ir', 'está', 'estabamos', 'estamos', 'estan', 'iban', 'igual', 'incluso', 'ir', 'acia', 'hacia', 'ha', 
+    'había', 'había', 'asta', 'hice', 'izo', 'hizo', 'la', 'las', 'lo', 'los', 'más', 'mas', 'menos', 'me', 'mí', 
+    'misma', 'mismas', 'mismo', 'mismos', 'muy', 'mucho', 'muxo', 'muchos', 'nada', 'ni', 'no', 'nos', 'nosotros', 
+    'o', 'otra', 'otras', 'otros', 'para', 'pero', 'poco', 'por', 'porq', 'porque', 'porqué', 'cual', 'cuál', 
+    'cuales', 'cuáles', 'cuando', 'cuándo', 'que', 'qué', 'se', 'sin', 'sobre', 'soy', 'sólo', 'solo', 'su', 'sus', 
+    'también', 'tambn', 'tan', 'tanta', 'tantas', 'tanto', 'tantos', 'tiene', 'tu', 'tuya', 'tuyas', 'tuyo', 'tuyos', 
+    'un', 'una', 'unas', 'uno', 'unos', 'ya'
+]
 
 
 
--- python/MyDataAnalysisProject/utils/fuctions.py
+++ python/MyDataAnalysisProject/utils/fuctions.py
@@ -2,6 +2,7 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 from collections import Counter
 import pandas as pd
+import emoji
 import re
 
 
@@ -25,10 +26,8 @@ def get_video_data(video_id, youtube):
         id=video_id
     )
     response = request.execute()
-
     # video data response
     video_data = response['items'][0]
-
     # Extract relevant data
     video_info = {
         'video_id': video_data['id'],
@@ -64,7 +63,7 @@ def get_comments_data(video_id, youtube):
             videoId=video_id,
             maxResults=100,
             pageToken=next_page_token
-  # Use next_page_token for pagination
+  # use next_page_token for pagination
         )
         response = request.execute()
         for item in response['items']:
@@ -85,31 +84,46 @@ def get_comments_data(video_id, youtube):
 
 # Function to scrape youtube videos data and comments data using YouTube Data 3.0 API
 def collect_youtube_video_data(videos_ids, youtube):
-    # Arrays to save data
+    # arrays to save data
     videos_data = []
     comments_data = []
-
-    # Scrape video data
+    # collect video data
     for id in videos_ids:
         # retrive data
         video_data = get_video_data(video_id=id, youtube=youtube)
         comments = get_comments_data(video_id=id, youtube=youtube)
-
         # save data
         videos_data.append(video_data)
         comments_data.extend(comments) 
-
+    # save data in dataframes
     videos_data = pd.DataFrame(videos_data)
     comments_data = pd.DataFrame(comments_data)
     return videos_data, comments_data
 
 
-# Function to clean text from comments
-def clean_comments_text(text):
-    # Remove emojis
+# Function to apply a  basic cleanning process in comments text
+def basic_text_cleaning(text):
+    # convert text to lowercase
+    text = text.lower()
+    # remove urls
+    text = re.sub(r'http\S+|www.\S+', '', text)  
+    # remove mentions
+    text = re.sub(r'@\w+', '', text)
+    # remove Hashtags  
+    text = re.sub(r'#\w+', '', text)
+    # normalize blank space use
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+
+# Function to apply specific cleanning process in text for common words analysis
+def common_words_cleanning(text, stopwords):
+    # basic cleanning
+    text = basic_text_cleaning(text)
+    # remove emojis, symbols and pictograms
     emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emojis
-                           u"\U0001F300-\U0001F5FF"  # simbols and pictograms
+                           u"\U0001F600-\U0001F64F"  
+                           u"\U0001F300-\U0001F5FF"  
                            u"\U0001F680-\U0001F6FF"  
                            u"\U0001F700-\U0001F77F"  
                            u"\U0001F780-\U0001F7FF"
@@ -121,27 +135,27 @@ def clean_comments_text(text):
                            u"\U000024C2-\U0001F251" 
                            "]+", flags=re.UNICODE)
     text = emoji_pattern.sub(r'', text)
-    
-    # Remove urls
-    url_pattern = re.compile(r'https?://\S+|www\.\S+')
-    text = url_pattern.sub(r'', text)
-
-    # Remove spetial characters
-    text = re.sub(r'[^A-Za-z0-9áéíóúÁÉÍÓÚñÑ ]', '', text)
-    text = re.sub(r'\s+', ' ', text).strip()
-    return text
-
-
-# Function to remove stopwords and clean text based on this technique
-def remove_stopwords(text, stopwords):
-    # lowercase text
-    text = text.lower()
-    # Dvide text and remove stopwords
+    # remove special characters
+    text = re.sub(r'[^a-z0-9áéíóúÁÉÍÓÚñÑ ]', '', text)
+    # divide text into words
     words = text.split()
+    # remove stopwords
     words = [word for word in words if word not in stopwords]
     return ' '.join(words)
 
 
+# Function to apply specific cleanning process in text for sentiment analysis
+def sentiment_analysis_cleanning(text):
+    # basic cleanning
+    text = basic_text_cleaning(text)
+    # convert emojis into text
+    text = emoji.demojize(text)
+    # remove special characters, except relevants for sentiment analysis
+    text = re.sub(r'[^a-zA-Z0-9\s¡!¿?.,;]', '', text)
+    # Dvide text and remove stopwords
+    return text
+
+"""
 # Function to get more frequent words
 def get_most_common_words(comments, stopwords, n=50):
     all_words = []
@@ -151,7 +165,7 @@ def get_most_common_words(comments, stopwords, n=50):
     
     word_count = Counter(all_words)
     return dict(word_count.most_common(n))
-
+"""
 
 # Function to generate a word cloud
 def generate_wordcloud(words_dict, title):
